edd26efef72f19b9eda066bd59c696e1dfda184c
joiky <joiky@163.com>
2020-12-17 05:54:15 +0000

    fix: LoadYamlPath

--- yaml.go
+++ yaml.go
@@ -1,6 +1,7 @@
 package bzconf
 
 import (
+	"errors"
 	"io/ioutil"
 
 	"gopkg.in/yaml.v2"
@@ -13,10 +14,20 @@ func LoadYaml(file string, out interface{}) error {
 		return err
 	}
 
-	return yaml.Unmarshal(data, out)
+	return LoadYamlBuffer(data, out)
+}
+
+// LoadYamlBuffer load yaml buffer, and decode to out
+func LoadYamlBuffer(buffer []byte, out interface{}) error {
+	if len(buffer) == 0 {
+		return errors.New("empty buffer")
+	}
+
+	return yaml.Unmarshal(buffer, out)
 }
 
 // LoadYamlPath load yaml file, and decode to out
 func LoadYamlPath(path string, out interface{}, opts ...*OptionsList) error {
+	opts = append(opts, &OptionsList{IncludeDir: false})
 	return loadPath(path, out, LoadYaml, opts...)
 }
